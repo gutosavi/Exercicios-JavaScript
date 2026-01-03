1538ae1fe2f81233f9f9d9c506be1acf6f8ac405
Gustavo Savi <gutosavi@hotmail.com>
2026-01-02 21:23:51 -0300

    Update main.js

--- assets_3/js/main.js
+++ assets_3/js/main.js
@@ -1,9 +1,6 @@
 function relogio (){
 
     const relogio = document.querySelector('.relogio');
-    const iniciar = document.querySelector('.iniciar');
-    const pausar = document.querySelector('.pausar');
-    const zerar = document.querySelector('.zerar')
     let segundos = 0;
     let timer;
 
