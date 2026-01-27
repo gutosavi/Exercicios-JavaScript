a59d0add04321447c49e422aa2754a4092cb13c9
Gustavo Savi <gutosavi@hotmail.com>
2026-01-26 21:30:31 -0300

    update main.js

--- assets6/js/main.js
+++ assets6/js/main.js
@@ -19,14 +19,46 @@ function ValidaCPF(cpf){
         get: function(){
             return cpf.replace(/\D+/g, '');
         }
-    })
+    });
 };
 
 ValidaCPF.prototype.valida = function(){
     if(typeof this.cpfLimpo === 'undefined') return false;
-    return true;
-}
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCPF = cpfParcial + digito1 + digito2;
+    return novoCPF === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial){
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const digito = cpfArray.reduce((acc, valor) => {
+        acc += (regressivo * Number(valor));
+        regressivo--;
+        return acc;
+    }, 0)
+    const resto = digito % 11;
+    return resto < 2 ? '0' : String(11 - resto); // (total % 11) - 11 
+};
+
+ValidaCPF.prototype.isSequencia = function(){
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+};
 
 const cpf = new ValidaCPF('009.345.390-65');
-console.log(cpf.valida());
+
+if(cpf.valida()){
+    console.log('CPF válido');
+} else {
+    console.log('CPF inválido');
+};
+
+
 
