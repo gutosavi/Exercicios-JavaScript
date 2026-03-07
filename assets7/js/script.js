554c683a16e027ebdf67dae4fbfdf78ba96d1669
Gustavo Savi <gutosavi@hotmail.com>
2026-03-07 13:21:04 -0300

    fix: exercício de manipulação do dom p.2

--- assets7/js/script.js
+++ assets7/js/script.js
@@ -3,7 +3,33 @@ const cssText = document.querySelector(".css");
 const btn = document.querySelector(".btn");
 
 function handleChange(e) {
-  console.log(e.target.value);
+  if (e.target.name === "backgroundColor") {
+    btn.style.backgroundColor = e.target.value;
+  } else if (e.target.name === "texto") {
+    btn.innerHTML = e.target.value;
+  } else if (e.target.name === "color") {
+    btn.style.color = e.target.value;
+  } else if (e.target.name === "height") {
+    btn.style.height = e.target.value + "px";
+  } else if (e.target.name === "width") {
+    btn.style.width = e.target.value + "px";
+  } else if (e.target.name === "border") {
+    btn.style.border = e.target.value;
+  } else if (e.target.name === "borderRadius") {
+    btn.style.borderRadius = e.target.value + "px";
+  } else if (e.target.name === "fontFamily") {
+    btn.style.fontFamily = e.target.value;
+  } else if (e.target.name === "fontSize") {
+    btn.style.fontSize = e.target.value + "px";
+  }
+
+  showStyle(btn);
+}
+
+function showStyle(element) {
+  const css = document.querySelector(".css");
+  const estilos = element.style.cssText.split("; ").join("; <br>");
+  css.innerHTML = estilos;
 }
 
 controles.addEventListener("change", handleChange);
